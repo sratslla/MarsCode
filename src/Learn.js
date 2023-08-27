6116e89d4c62434705982a642793fd7b93f86615
sratslla <shubhgaur.cse24@jecrc.ac.in>
2023-08-27 16:35:25 +0530

    first-commit

---
+++ src/Learn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Learn = () => {
+  return (
+    <div>
+        <div>Learn window it is.</div>
+    </div>
+  )
+}
+
+export default Learn
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import Learn from './Learn';
+import Home from './Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "learn",
+        element: <Learn />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
