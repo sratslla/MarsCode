90fc1561d011369ec2afbda114de943105155a4b
Alvaro Torres <atorr1020@gmail.com>
2023-08-29 21:16:06 -0500

    added notfound component for any route that doesn't exist and added styling for the page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import Learn from './pages/Learn';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound/NotFound';
 import C_Editor from './pages/Collaborative_Coding/C_Editor';
 import C_Home from './pages/Collaborative_Coding/C_Home';
 
 const router = createBrowserRouter([
+  {
+    path: "/*",
+    element: <NotFound />,
+  },
   {
     path: "/",
     element: <Home />,
