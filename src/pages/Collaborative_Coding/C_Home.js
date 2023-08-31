959d5e2a89ebd73a685fc4146b43f438f3b777ff
mbp15 <mbp15@users-iMac-2.hitronhub.home>
2023-08-30 20:18:14 -0700

    updated frontend

--- src/pages/Collaborative_Coding/C_Home.js
+++ src/pages/Collaborative_Coding/C_Home.js
@@ -1,10 +1,15 @@
 import "./C_Home.css";
 import C_Navbar from "./C_Navbar/C_Navbar";
-
+import Slider from "./C_Switch/Slider";
 import React from "react";
+import Paragraph from "./C_Paragraph/Paragraph";
 
 export default function C_Home() {
   return (
-    <C_Navbar />
+    <>
+      <C_Navbar />
+      <Slider />
+      <Paragraph />
+    </>
   );
 }
---
+++ src/pages/Collaborative_Coding/C_Paragraph/Paragraph.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Paragraph.css"
+
+export default function Paragraph() {
+  return (
+    <div>
+        <p className='part1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dignissim laoreet lacinia. In ex orci, hendrerit at egestas nec, faucibus quis orci. Ut aliquet mi nulla, sed ornare libero semper et. Duis ornare eleifend pharetra. Praesent fermentum lacus quis ipsum cursus bibendum. Vivamus vel feugiat lectus, ac facilisis lectus. Maecenas blandit justo ex, eu suscipit lacus pellentesque placerat. Cras lectus lacus, feugiat in tempor pulvinar, cursus feugiat nisl. Ut et consequat odio. Curabitur nec arcu quis velit molestie finibus vel eget ex.</p>
+        <p className='part2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dignissim laoreet lacinia. In ex orci, hendrerit at egestas nec, faucibus quis orci. Ut aliquet mi nulla, sed ornare libero semper et. Duis ornare eleifend pharetra. Praesent fermentum lacus quis ipsum cursus bibendum. Vivamus vel feugiat lectus, ac facilisis lectus. Maecenas blandit justo ex, eu suscipit lacus pellentesque placerat. Cras lectus lacus, feugiat in tempor pulvinar, cursus feugiat nisl. Ut et consequat odio. Curabitur nec arcu quis velit molestie finibus vel eget ex.</p>
+    </div>
+  )
+}
