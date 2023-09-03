c6b1773b0368b070b3b9218f6598be93e839fb70
mbp15 <mbp15@users-iMac-2.hitronhub.home>
2023-09-02 20:39:25 -0700

    updated UI and added demo footer

--- src/pages/Collaborative_Coding/C_Home.js
+++ src/pages/Collaborative_Coding/C_Home.js
@@ -3,6 +3,7 @@ import C_Navbar from "./C_Navbar/C_Navbar";
 import Slider from "./C_Switch/Slider";
 import React from "react";
 import Paragraph from "./C_Paragraph/Paragraph";
+import Footer from "./C_Footer/Footer";
 
 export default function C_Home() {
   return (
@@ -10,6 +11,7 @@ export default function C_Home() {
       <C_Navbar />
       <Slider />
       <Paragraph />
+      <Footer />
     </>
   );
 }
---
+++ src/pages/FootDescript/FootDescription.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FootDescription = () => {
+  return (
+    <div>
+        <h1>Like a content?</h1>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+    </div>
+  );
+}
+
+export default FootDescription;
